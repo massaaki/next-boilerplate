8317d9bf9108f34646ff745dddcf5dd1f7c23780
Mauricio Yochizumi <mauricioyochizumi@MacBook-Pro-de-Mauricio-7.local>
2020-11-18 21:24:10 -0300

    feat(tests: Updated <Main /> tests

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -9,4 +9,10 @@ describe('<Main />', () => {
     ).toBeInTheDocument()
     expect(container.firstChild).toMatchSnapshot()
   })
+
+  it('should render the background color correctly', () => {
+    const { container } = render(<Main />)
+
+    expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' })
+  })
 })
